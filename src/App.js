6e8bb5cc1378c6b92372db48f8b7677f5f030f78
Dimas Aulia Fachrudin <dimasauliafachrudin@gmail.com>
2022-05-26 09:20:26 +0700

    add filter functionality

--- src/App.js
+++ src/App.js
@@ -2,16 +2,44 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import SearchForm from "./SearchForm";
 import CarBucket from "./CarBusket";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
+  const [cars, setCars] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const renderCar = async () => {
+    const carsData = await axios.get("http://localhost:8080/cars");
+    setCars(carsData.data);
+    setIsLoading(false);
+  };
+
+  const searchCar = async (option) => {
+    const filterDate = new Date(`${option.date}T${option.time}`);
+    console.log(filterDate);
+    const carsData = await axios.get("http://localhost:8080/cars");
+    const filterdCard = carsData.data.filter(
+      (car) =>
+        new Date(car.availableAt).getTime() === filterDate.getTime() &&
+        car.capacity >= Number(option.capacity) &&
+        car.available === true
+    );
+    setCars(filterdCard);
+    setIsLoading(false);
+  };
+
+  useEffect(() => renderCar, []);
+
   return (
     <div className="App">
       <Navbar />
 
       <Hero />
 
-      <SearchForm />
+      <SearchForm searchCar={searchCar} />
 
-      <CarBucket />
+      <CarBucket isLoading={isLoading} cars={cars} />
     </div>
   );
 }
--- src/SearchForm.js
+++ src/SearchForm.js
@@ -2,9 +2,11 @@ import arr_up from "./icons/icon_arr_up.svg";
 import calender from "./icons/icon_calender.png";
 import clock from "./icons/icon_clock.svg";
 import capacity from "./icons/icon_capacity.svg";
-import close from "./icons/icon_close.svg"
-import {useState} from "react"
-const SearchForm = () => {
+import close from "./icons/icon_close.svg";
+import { useState } from "react";
+
+const SearchForm = ({ searchCar }) => {
+  const [formData, setFormData] = useState({});
 
   const [driverOption, setDriverOption] = useState(""); // state untuk membuka UI menu
   const [driverIcon, setDriverIcon] = useState(arr_up); // state untuk mengubah Icon
@@ -17,49 +19,50 @@ const SearchForm = () => {
   const openDriverOption = () => {
     setDriverOption("form-active");
     setDriverIcon(close);
-  }
+  };
 
   const closeDriverOption = () => {
     setDriverOption("");
     setDriverIcon(arr_up);
-  }
+  };
 
   const changeDriverOption = () => {
     if (driverOption === "") {
-      openDriverOption()
-    }else{
-      closeDriverOption()
+      openDriverOption();
+    } else {
+      closeDriverOption();
     }
-  }
+  };
 
   const fillDriverForm = (data) => {
     setFillDriverOption(data);
-    setDriverOption("");
+    setFormData({ ...formData, tipe: data });
     closeDriverOption();
-  }
+  };
 
   const openPickUpTime = () => {
     setPickUpTime("form-active");
     setPickUpTimeIcon(close);
-  }
+  };
 
   const closePickUpTime = () => {
     setPickUpTime("");
     setPickUpTimeIcon(clock);
-  }
+  };
 
   const changePickUpTime = () => {
     if (pickUpTime === "") {
-      openPickUpTime()
-    }else{
-      closePickUpTime()
+      openPickUpTime();
+    } else {
+      closePickUpTime();
     }
-  }
+  };
 
   const fillPickUpForm = (data) => {
     setFillPickUpTime(data);
+    setFormData({ ...formData, time: data });
     closePickUpTime();
-  }
+  };
 
   return (
     <section className="cars-service">
@@ -76,15 +79,38 @@ const SearchForm = () => {
                 id="tipe"
                 aria-describedby="emailHelp"
                 value={fillDriverOption}
-                onClick = {()=>changeDriverOption()}
+                onClick={() => {
+                  changeDriverOption();
+                }}
               />
               <span className="input-group-text">
-                <img src={driverIcon} alt="Panah" className="inp-arr" onClick = {()=> changeDriverOption() }/>
+                <img
+                  src={driverIcon}
+                  alt="Panah"
+                  className="inp-arr"
+                  onClick={() => changeDriverOption()}
+                />
               </span>
 
-              <div className={`input-option bg-white ${driverOption}`}id="tipe_driver--option">
-                <p className="fs-6 p-2 mb-2 input-item" onClick={()=> fillDriverForm("Dengan Sopir") }>Dengan Sopir</p>
-                <p className="fs-6 p-2 input-item" onClick={()=> fillDriverForm("Tanpa Sopir") }>Tanpa Sopir</p>
+              <div
+                className={`input-option bg-white ${driverOption}`}
+                id="tipe_driver--option"
+              >
+                <p
+                  className="fs-6 p-2 mb-2 input-item"
+                  onClick={(e) => {
+                    fillDriverForm("Dengan Sopir");
+                    setFormData({ ...formData, tipe: "Dengan Sopir" });
+                  }}
+                >
+                  Dengan Sopir
+                </p>
+                <p
+                  className="fs-6 p-2 input-item"
+                  onClick={() => fillDriverForm("Tanpa Sopir")}
+                >
+                  Tanpa Sopir
+                </p>
               </div>
             </div>
           </div>
@@ -99,6 +125,9 @@ const SearchForm = () => {
                 className="form-control"
                 id="tanggal"
                 aria-describedby="emailHelp"
+                onChange={(e) =>
+                  setFormData({ ...formData, date: e.target.value })
+                }
               />
               <span className="input-group-text">
                 <img src={calender} alt="Icon Kalender" />
@@ -116,28 +145,88 @@ const SearchForm = () => {
                 className="form-control"
                 id="waktu"
                 autoComplete="off"
-                onClick={()=> changePickUpTime() }
+                onClick={() => changePickUpTime()}
                 value={fillPickUpTime}
               />
               <span className="input-group-text">
-                <img src={pickUpTimeIcon} alt="Icon Jam" className="clock" onClick={()=> changePickUpTime() } />
+                <img
+                  src={pickUpTimeIcon}
+                  alt="Icon Jam"
+                  className="clock"
+                  onClick={() => changePickUpTime()}
+                />
               </span>
 
               <div
                 className={`input-option bg-white waktu-form ${pickUpTime}`}
                 id="tipe_waktu--option"
               >
-                <p className="fs-6 p-2 mb-2 waktu-item" onClick={()=>fillPickUpForm("01:00")} >01:00 WIB</p>
-                <p className="fs-6 p-2 mb-2 waktu-item" onClick={()=>fillPickUpForm("02:00")} >02:00 WIB</p>
-                <p className="fs-6 p-2 mb-2 waktu-item" onClick={()=>fillPickUpForm("03:00")} >03:00 WIB</p>
-                <p className="fs-6 p-2 mb-2 waktu-item" onClick={()=>fillPickUpForm("04:00")} >04:00 WIB</p>
-                <p className="fs-6 p-2 mb-2 waktu-item" onClick={()=>fillPickUpForm("05:00")} >05:00 WIB</p>
-                <p className="fs-6 p-2 mb-2 waktu-item" onClick={()=>fillPickUpForm("06:00")} >06:00 WIB</p>
-                <p className="fs-6 p-2 mb-2 waktu-item" onClick={()=>fillPickUpForm("07:00")} >07:00 WIB</p>
-                <p className="fs-6 p-2 mb-2 waktu-item" onClick={()=>fillPickUpForm("08:00")} >08:00 WIB</p>
-                <p className="fs-6 p-2 mb-2 waktu-item" onClick={()=>fillPickUpForm("09:00")} >09:00 WIB</p>
-                <p className="fs-6 p-2 mb-2 waktu-item" onClick={()=>fillPickUpForm("10:00")} >10:00 WIB</p>
-                <p className="fs-6 p-2 mb-2 waktu-item" onClick={()=>fillPickUpForm("15:00")} >15:00 WIB</p>
+                <p
+                  className="fs-6 p-2 mb-2 waktu-item"
+                  onClick={() => fillPickUpForm("01:00")}
+                >
+                  01:00 WIB
+                </p>
+                <p
+                  className="fs-6 p-2 mb-2 waktu-item"
+                  onClick={() => fillPickUpForm("02:00")}
+                >
+                  02:00 WIB
+                </p>
+                <p
+                  className="fs-6 p-2 mb-2 waktu-item"
+                  onClick={() => fillPickUpForm("03:00")}
+                >
+                  03:00 WIB
+                </p>
+                <p
+                  className="fs-6 p-2 mb-2 waktu-item"
+                  onClick={() => fillPickUpForm("04:00")}
+                >
+                  04:00 WIB
+                </p>
+                <p
+                  className="fs-6 p-2 mb-2 waktu-item"
+                  onClick={() => fillPickUpForm("05:00")}
+                >
+                  05:00 WIB
+                </p>
+                <p
+                  className="fs-6 p-2 mb-2 waktu-item"
+                  onClick={() => fillPickUpForm("06:00")}
+                >
+                  06:00 WIB
+                </p>
+                <p
+                  className="fs-6 p-2 mb-2 waktu-item"
+                  onClick={() => fillPickUpForm("07:00")}
+                >
+                  07:00 WIB
+                </p>
+                <p
+                  className="fs-6 p-2 mb-2 waktu-item"
+                  onClick={() => fillPickUpForm("08:00")}
+                >
+                  08:00 WIB
+                </p>
+                <p
+                  className="fs-6 p-2 mb-2 waktu-item"
+                  onClick={() => fillPickUpForm("09:00")}
+                >
+                  09:00 WIB
+                </p>
+                <p
+                  className="fs-6 p-2 mb-2 waktu-item"
+                  onClick={() => fillPickUpForm("10:00")}
+                >
+                  10:00 WIB
+                </p>
+                <p
+                  className="fs-6 p-2 mb-2 waktu-item"
+                  onClick={() => fillPickUpForm("15:00")}
+                >
+                  15:00 WIB
+                </p>
               </div>
             </div>
           </div>
@@ -154,6 +243,9 @@ const SearchForm = () => {
                 autoComplete="off"
                 min="1"
                 max="9"
+                onChange={(e) =>
+                  setFormData({ ...formData, capacity: e.target.value })
+                }
               />
               <span className="input-group-text">
                 <img src={capacity} alt="Icon Penumpang" />
@@ -162,7 +254,12 @@ const SearchForm = () => {
           </div>
 
           <div className="p-2 form-item">
-            <button type="button" className="btn btn-submit" id="cari">
+            <button
+              type="button"
+              className="btn btn-submit"
+              id="cari"
+              onClick={() => searchCar(formData)}
+            >
               Cari
             </button>
           </div>
